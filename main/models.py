6d93b5e5f5b8ff3eea1faa9e3620376bfc63b864
Peter Henry <phenry@caltech.edu>
2015-09-14 18:53:41 -0600

    some models created before cms attempt

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+
+
+class Blog(models):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField(null=True, blank=True)
