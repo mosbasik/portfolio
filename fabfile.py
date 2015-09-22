92709fe73aadc12495cc47aa758744b922b6b417
Peter Henry <phenry@caltech.edu>
2015-09-22 15:27:43 -0600

    added fabric for easier deployment

---
+++ fabfile.py
@@ -0,0 +1,41 @@
+from __future__ import with_statement
+from contextlib import contextmanager as _contextmanager
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.use_ssh_config = True
+env.hosts = 'peterhenry'
+
+env.directory = '/sites/projects/portfolio'
+env.activate = 'source /sites/virtualenvs/portfolio/bin/activate'
+
+
+@_contextmanager
+def virtualenv():
+    with cd(env.directory):
+        with prefix(env.activate):
+            yield
+
+
+def pull():
+    '''Pull the newest code from the repository.'''
+    with cd(env.directory):
+        # sudo('git pull', user='www-data')
+        run('git pull')
+
+
+def collect_static():
+    '''Collect all static files and put in static root for Apache.'''
+    with virtualenv():
+        run('./manage.py collectstatic --noinput')
+
+
+def restart_apache():
+    '''Restart Apache daemon to begin serving changes to website.'''
+    run('service apache2 restart')
+
+
+def deploy():
+    pull()
+    collect_static()
+    restart_apache()
